c9a94a0a2d203addcb3c90727af6e7307de8ca5c
santhoshr2204 <santhoshr2204@gmail.com>
2025-12-10 00:34:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,19 +18,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-x=input("Temp,Humidity,WindSpeed : ").split(',')
 
-def predict(x):
-    y=[float(q) for q in x]
-    model=WeatherNN()
-    X=torch.tensor([y],dtype=torch.float32)
-    prediction = model(X)
-    print('Prediction: ',prediction)
-    if prediction<=0.15:
-        print('Sunny')
-    elif prediction>=0.95:
-        print('Rainy')
-    else:
-        print('unable to say')
-
-predict(x)
